4a65e166b4a4cb0e4e292e223838fc7034751e52
Justin <justinkaashoek@college.harvard.edu>
2018-07-12 13:07:34 -0400

    add production settings, setup apps

---
+++ psiproject/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'i9m7!1oij3-yx8o0xq09j4ow_^t2hazhyt#t@r^kr6n1ad3!*t'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
---
+++ psiproject/settings/production.py
@@ -0,0 +1,5 @@
+from base import *
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DEBUG = False
