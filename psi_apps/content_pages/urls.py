dbeb0f0018c7cfd06610f88c5920d0ed7c866817
Raman Prasad <raman_prasad@harvard.edu>
2018-09-24 10:26:23 -0400

    Remove individual page templates and replace with a general url that serves files from the /templates/content directory

--- psi_apps/content_pages/urls.py
+++ psi_apps/content_pages/urls.py
@@ -5,18 +5,28 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    re_path(r'^psiIntroduction$', views.psiIntroduction, name='psiIntroduction'),
-    re_path(r'^psiOpen$', views.psiOpen, name='psiOpen'),
-    re_path(r'^psiOpenPrototype$', views.psiOpenPrototype, name='psiOpenPrototype'),
-    re_path(r'^getData$', views.getData, name='getData'),
-    re_path(r'^getXML$', views.getXML, name='getXML'),
 
     # for k8s healthchecks
     re_path(r'^monitoring/alive$',
             views.view_monitoring_alive,
             name='view_monitoring_alive'),
 
+    # Test mode, serve the test metadata JSON and XML files
+    #
+    re_path(r'^getData$', views.getData, name='getData'),
+    re_path(r'^getXML$', views.getXML, name='getXML'),
+
+    # serve templates from the "/templates/content"
+    #    directory in this project
+    #
+    re_path(r'^content/(?P<page_name>[\w\-\._]+)$',
+            views.view_content_page,
+            name='viewContentPage'),
+
+    # Serve the home page
+    #
     re_path(r'^', views.interface, name='interface'),
+
     # re_path(r'^%s' % views.ROOK_FILES_PATH, # 'rook-files/'
     # 	views.view_rook_file_passthrough,
     # 	name='view_rook_file_passthrough'),
